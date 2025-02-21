f2afbb2a8c6038d8958779d3230acc3815c6477a
Irina <ouaaey@gmail.com>
2025-02-21 12:08:06 +0400

    2.1.4

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Autowired
+    private Needle7 needle7;
+
+    @Override
+    public String toString() {
+        return needle7.toString();
+    }
 }
